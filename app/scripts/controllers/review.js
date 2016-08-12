3187946a676ee65be5c8da1046774c957380b74c
Linda Hellborg <linda.hellborg@gmail.com>
2016-08-12 09:11:08 +0200

    add todays date in date selector

--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -12,6 +12,13 @@ angular.module('restaurantReviewApp')
 
   	var vm = this;
 
-  	vm.todaysDate = new Date(2013, 9, 22)
+// copied from http://stackoverflow.com/questions/6982692/html5-input-type-date-default-value-to-today/13052187#13052187
+  	Date.prototype.toDateInputValue = (function() {
+	    var local = new Date(this);
+	    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+	    return local.toJSON().slice(0,10);
+	});
+
+	vm.todaysDate = new Date().toDateInputValue();
 
   });
